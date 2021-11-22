d5b43549ec73d80f505a920317c184bab984ec06
bilecky <51762310+bilecky@users.noreply.github.com>
2021-11-22 23:03:03 +0100

    footer complete

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
 
+const navBtnBars = 
+
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
 
