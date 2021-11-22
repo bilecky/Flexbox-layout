da8ba45926149cb7bc56a25490cdef0d18f226a9
bilecky <51762310+bilecky@users.noreply.github.com>
2021-11-22 23:30:14 +0100

    added js to nav button

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
 
-const navBtnBars = 
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
-
+	navBtnBars.classList.remove('black-bars-color')
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			nav.classList.remove('nav--active')
@@ -20,13 +22,35 @@ const handleNavItemsAnimation = () => {
 	allNavItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
 		item.style.animationDelay = `.${delayTime}s`
-        delayTime++
+		delayTime++
 	})
 }
 const deleteAnimation = () => {
-    allNavItems.forEach(item => {
-        item.classList.remove('nav-items-animation')
-    })
+	allNavItems.forEach(item => {
+		item.classList.remove('nav-items-animation')
+	})
 }
 
+// _________FOOTER
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+// Section observer (black/white)
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+handleCurrentYear()
 navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
